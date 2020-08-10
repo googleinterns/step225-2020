63daee00f84728650580d536f8712474195030b2
Veronika Nechaieva <vnechaieva@google.com>
2020-08-10 10:03:11 +0300

    Item selection

--- gTea/app/src/main/java/com/nechaieva/gtea/MenuAdapter.java
+++ gTea/app/src/main/java/com/nechaieva/gtea/MenuAdapter.java
@@ -1,5 +1,6 @@
 package com.nechaieva.gtea;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +11,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
     private String[] data;
+    private int selectedPos = RecyclerView.NO_POSITION;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
         public TextView view;
 
         public ViewHolder(View v) {
             super(v);
             view = v.findViewById(R.id.textView);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Redraw the old selection and the new
+            notifyItemChanged(selectedPos);
+            selectedPos = getLayoutPosition();
+            notifyItemChanged(selectedPos);
+            Log.i("RecycleView position", String.valueOf(selectedPos));
         }
     }
 
@@ -30,16 +42,15 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
                                          int viewType) {
         View rootView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item, parent, false);
-        //TextView v = (TextView) rootView.findViewById(R.id.textView);
-        //...
         return new ViewHolder(rootView);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.view.setText(data[position]);
-
+        holder.itemView.setSelected(selectedPos == position);
     }
+
 
     @Override
     public int getItemCount() {
