41f68c968fa8b4ec04d05a83020cbd28bf91d30d
Veronika Nechaieva <vnechaieva@google.com>
2020-09-14 20:11:31 +0300

    Add Levenshtein calculator

--- gTea/app/src/main/java/com/nechaieva/gtea/utils/LevenshteinDistCalculator.java
+++ gTea/app/src/main/java/com/nechaieva/gtea/utils/LevenshteinDistCalculator.java
@@ -1,12 +1,45 @@
 package com.nechaieva.gtea.utils;
 
 public class LevenshteinDistCalculator {
+    static int[][] dp;
 
     /**
      * Calculates given distance between two given strings.
      * Complexity: O(a.len * b.len)
      */
     static int stringDistance(String a, String b) {
-        return 0;
+        final int al = a.length(), bl = b.length();
+        dp = new int[al][bl];
+
+        for (int y = 0; y < al; y++) {
+            dp[y][0] = y;
+        }
+        for (int x = 1; x < bl; x++) {
+            dp[0][x] = x;
+        }
+        for (int y = 1; y < al; y++) {
+            for (int x = 1; x < bl; x++) {
+                int charEquals = a.charAt(y) != b.charAt(x) ? 1 : 0;
+                dp[y][x] = min(dp[y][x-1] + 1,
+                               dp[y-1][x] + 1,
+                               dp[y-1][x-1] + charEquals);
+            }
+        }
+        printArray(al, bl);
+        return dp[al-1][bl-1];
+    }
+
+    private static int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+
+    private static void printArray(int ySize, int xSize) {
+        for (int y = 0; y < ySize; y++) {
+            for (int x = 0; x < xSize; x++) {
+                System.out.print(dp[y][x] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
---
+++ gTea/app/src/test/java/com/nechaieva/gtea/utils/LevenshteinDistCalculatorTest.java
@@ -0,0 +1,32 @@
+package com.nechaieva.gtea.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LevenshteinDistCalculatorTest {
+
+    @Test
+    public void testLetterMissing() {
+        test( 1,"hello", "hell");
+    }
+
+    @Test
+    public void testLetterAdded() {
+        test( 1,"hell", "hello");
+    }
+
+    @Test
+    public void testLetterChanged() {
+        test( 1,"hell", "hall");
+    }
+
+    @Test
+    public void testClassicalExample() {
+        test( 6,"polynomial", "exponential");
+    }
+
+    private void test(int dist, String a, String b) {
+        assertEquals( dist, LevenshteinDistCalculator.stringDistance(a, b));
+    }
+}
